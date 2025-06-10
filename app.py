e5ea595b34863f1f637be522d48f04f20cd15054
Noga332 <nogaitzig.daniel@web.de>
2025-06-10 11:08:37 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/bangkok')
+def bangkok():
+    return render_template('bangkok.html')
+
+@app.route('/huahin')
+def huahin():
+    return render_template('huahin.html')
+
+@app.route('/seka')
+def seka():
+    return render_template('seka.html')
+
+@app.route('/pattaya')
+def pattaya():
+    return render_template('pattaya.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
